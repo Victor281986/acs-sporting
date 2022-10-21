472684df9a7ca4a55f24cb98d68e8505cce1d11a
Victor281986 <115946134+Victor281986@users.noreply.github.com>
2022-10-21 08:31:19 +0300

    Create Tournament.js

---
+++ src/components/Tournament.js
@@ -0,0 +1,20 @@
+import React from "react":
+
+export default function Tournament (props) {
+  const { name, images, mainImage } = props;
+
+  return (
+    <div className="tournament">
+	<div className="tournament-dropdown">
+	    <section className="tournament-wrapper">
+		<img src={mainImage}/>
+		<span>{name}</span>
+	    </section>
+	</div>
+
+	<div className="tournament-images">
+            {images}
+	</div>
+    </div>
+  );
+}
