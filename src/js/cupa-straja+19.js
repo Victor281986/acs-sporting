e5c955bc93782f6ac831271b7fc3813e959794c7
Victor281986 <115946134+Victor281986@users.noreply.github.com>
2022-10-21 08:12:59 +0300

    Create cupa-straja+19.js

---
+++ src/js/cupa-straja+19.js
@@ -0,0 +1,19 @@
+import cupaStraja1 from "../images/tournaments/cupa-craciunului-2021/1.jpg";
+import cupaStraja2 from "../images/tournaments/cupa-craciunului-2021/2.jpg";
+import cupaStraja3 from "../images/tournaments/cupa-craciunului-2021/3.jpg";
+import cupaStraja4 from "../images/tournaments/cupa-craciunului-2021/4.jpg";
+import cupaStraja5 from "../images/tournaments/cupa-craciunului-2021/5.jpg";
+import cupaStraja6 from "../images/tournaments/cupa-craciunului-2021/6.jpg";
+import cupaStraja7 from "../images/tournaments/cupa-craciunului-2021/7.jpg";
+
+const images = [
+  cupaCraciun1,
+  cupaCraciun2,
+  cupaCraciun3,
+  cupaCraciun4,
+  cupaCraciun5,
+  cupaCraciun6,
+  cupaCraciun7
+];
+
+export default images;
