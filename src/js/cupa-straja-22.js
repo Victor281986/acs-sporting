893eeaf42deae4cd0ac66d3aeeae50218f9f0891
Victor281986 <115946134+Victor281986@users.noreply.github.com>
2022-10-21 08:21:17 +0300

    Create cupa-straja-22.js

---
+++ src/js/cupa-straja-22.js
@@ -0,0 +1,38 @@
+import cupsStraja1 from "./images/tournaments/cupa-straja-2019/1.jpg";
+import cupsStraja2 from "./images/tournaments/cupa-straja-2019/2.jpg";
+import cupsStraja3 from "./images/tournaments/cupa-straja-2019/3.jpg";
+import cupsStraja4 from "./images/tournaments/cupa-straja-2019/4.jpg";
+import cupsStraja5 from "./images/tournaments/cupa-straja-2019/5.jpg";
+import cupsStraja6 from "./images/tournaments/cupa-straja-2019/6.jpg";
+import cupsStraja7 from "./images/tournaments/cupa-straja-2019/7.jpg";
+import cupsStraja8 from "./images/tournaments/cupa-straja-2019/8.jpg";
+import cupsStraja9 from "./images/tournaments/cupa-straja-2019/9.jpg";
+import cupsStraja10 from "./images/tournaments/cupa-straja-2019/10.jpg";
+import cupsStraja11 from "./images/tournaments/cupa-straja-2019/11.jpg";
+import cupsStraja12 from "./images/tournaments/cupa-straja-2019/12.jpg";
+import cupsStraja13 from "./images/tournaments/cupa-straja-2019/13.jpg";
+import cupsStraja14 from "./images/tournaments/cupa-straja-2019/14.jpg";
+import cupsStraja15 from "./images/tournaments/cupa-straja-2019/15.JPG";
+import cupsStraja16 from "./images/tournaments/cupa-straja-2019/16.JPG";
+
+
+const images = [
+  cupaStraja1,
+  cupaStraja2,
+  cupaStraja3,
+  cupaStraja4,
+  cupaStraja5,
+  cupaStraja6,
+  cupaStraja7,
+  cupaStraja8,
+  cupaStraja9,
+  cupaStraja10,
+  cupaStraja11,
+  cupaStraja12,
+  cupaStraja13,
+  cupaStraja14,
+  cupaStraja15,
+  cupaStraja16
+];
+
+export default images;
